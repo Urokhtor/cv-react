0b48bfb717a7b642dc9af7f7df7dca682d0970a5
Jere Teittinen <urokhtor1@gmail.com>
2018-04-06 14:14:21 +0300

    There can be const.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
-let path = require('path');
-let CleanWebpackPlugin = require('clean-webpack-plugin');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     resolve: {
