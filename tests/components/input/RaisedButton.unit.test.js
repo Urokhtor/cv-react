24774afcf8088d8f326fd137765ff63c21f2eb4c
Jere Teittinen <urokhtor1@gmail.com>
2018-04-09 09:09:34 +0300

    Added simple Jest setup file to reduce boilerplate due to having to set up Enzyme.

--- tests/components/input/RaisedButton.unit.test.js
+++ tests/components/input/RaisedButton.unit.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import RaisedButton from "../../../src/components/input/RaisedButton";
-import Enzyme from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 test('RaisedButton should not have executed callback if button has not been clicked', (done) => {
     let mockOnClick = jest.fn();
-    let wrapper = Enzyme.shallow(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
+    let wrapper = shallow(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
 
     expect(mockOnClick).not.toHaveBeenCalled();
 
@@ -16,7 +12,7 @@ test('RaisedButton should not have executed callback if button has not been clic
 
 test('RaisedButton should execute callback when button is clicked', (done) => {
     let mockOnClick = jest.fn();
-    let wrapper = Enzyme.shallow(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
+    let wrapper = shallow(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
 
     wrapper.find('button').simulate('click');
 
@@ -27,7 +23,7 @@ test('RaisedButton should execute callback when button is clicked', (done) => {
 
 test('RaisedButton should display text passed to it', (done) => {
     let mockOnClick = jest.fn();
-    let wrapper = Enzyme.mount(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
+    let wrapper = shallow(<RaisedButton text={'some-test-text'} onClick={mockOnClick}/>);
 
     expect(wrapper.find('button').text()).toEqual('some-test-text');
 
