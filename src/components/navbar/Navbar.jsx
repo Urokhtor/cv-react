aaaf11febf9b5543d88bc91f284e345aaea196a4
Jere Teittinen <urokhtor1@gmail.com>
2018-04-11 18:40:11 +0300

    This key was left from experimentations, but is not needed.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -52,7 +52,7 @@ export default class Navbar extends Component {
         return (
             <nav style={this.props.style} className={"navbar-container"}>
                 <div className="navbar-spaced flex-end">
-                    <div ref={this.homeRef} key={'home'} onClick={this.onClick.bind(this, this.homeRef, 'home')}>Home</div>
+                    <div ref={this.homeRef} onClick={this.onClick.bind(this, this.homeRef, 'home')}>Home</div>
                     <div ref={this.aboutRef} onClick={this.onClick.bind(this, this.aboutRef, 'about')}>About</div>
                     <div ref={this.experienceRef} onClick={this.onClick.bind(this, this.experienceRef, 'experience')}>Experience</div>
                     <div ref={this.contactRef} onClick={this.onClick.bind(this, this.contactRef, 'contact')}>Contact</div>
