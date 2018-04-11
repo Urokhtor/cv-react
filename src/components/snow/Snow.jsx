1d523bc71c71d8b723603fea08e6b1147227b26e
Jere Teittinen <urokhtor1@gmail.com>
2018-04-11 18:22:39 +0300

    Nasty extra space.

--- src/components/snow/Snow.jsx
+++ src/components/snow/Snow.jsx
@@ -66,7 +66,7 @@ export default class Snow extends Component {
 
         this.setState({
             particles: livingParticles,
-            lastTime : currentTime
+            lastTime: currentTime
         })
     }
 
